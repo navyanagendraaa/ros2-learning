d3aa73d4f5428f986f94247f9371ba01684da51e
navyanagendraaa <navyanagendraaa@gmail.com>
2026-09-15 15:55:03 +0200

    Add ROS2 Python publisher subscriber package

---
+++ publisher_subscriber_py/py_pubsub/subscriber.py
@@ -0,0 +1,25 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class MySubscriber(Node):
+    def __init__(self):
+        super().__init__('my_subscriber')
+        self.subscription = self.create_subscription(
+            String, 'my_topic', self.listener_callback, 10)
+        self.get_logger().info('Subscriber node started.')
+
+    def listener_callback(self, msg):
+        self.get_logger().info(f'Received: {msg.data}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MySubscriber()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
